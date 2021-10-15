fde5a91b42df8909a0fcdb06a4572d1f765c9aca
winston <denis.remacle@outlook.fr>
2021-10-15 13:25:38 +0200

    Added comments

--- ez-calc.c
+++ ez-calc.c
@@ -3,12 +3,12 @@
 #include <string.h>
 #include <math.h>
 
-
 char *formula;
 char expression[30];
 int parenthesis_count = 0, parenthesis_location[2], counter, formula_size, expression_size, sign_location = -1;
 
 void signInterpreter(char sign){
+    // if the sign is found proceed with calculation then replace formula (or part of it if there is parenthesis) with expression
     switch (sign) {
         case '^':
             puts("Exponant found !");
@@ -34,12 +34,14 @@ void signInterpreter(char sign){
 
 void signFinder(char sign){
     counter = 0;
-
+    // if we search for opening parenthesis search for both parenthesis
     if (sign == '('){
         while (counter<formula_size){
+            // if opening parenthesis found add counter to location[0] keep searching for opening parenthesis
             if (formula[counter] == '('){
                 parenthesis_location[0] = counter;
             }
+            // else if closing parenthesis found add counter to location[1] and stop searching
             else if (formula[counter] == ')'){
                 parenthesis_location[1] = counter + 1;
                 break;
@@ -47,16 +49,16 @@ void signFinder(char sign){
             counter++;
         }
     }
+    // If searching an operator search only for the operator
     else {
         while (counter<expression_size){
+            // if operator found send to interpretation
             if (expression[counter] == sign){
                 sign_location = counter;
                 signInterpreter(sign);
                 break;
             }
-            else {
-                counter++;
-            }
+            counter++;
         }
     }
 }
@@ -66,9 +68,18 @@ void priorityCalc(void){
     int i = 0;
     parenthesis_location[0] = -1;
 
+    // going thru the PEMDAS order
     signFinder('(');
+    // if parenthesis_location[0] is not -1 there is still parenthesis
     if (parenthesis_location[0] != -1){
         for(counter=parenthesis_location[0]; counter<parenthesis_location[1]; counter++){
+            expression[i++] = formula[counter];
+        }
+        expression[i] = '\0';
+    }
+    // else there is no parenthesis
+    else {
+        for (counter = 0; counter>formula_size; counter++){
             expression[i++] = formula[counter];
         }
         expression[i] = '\0';
@@ -83,11 +94,11 @@ void priorityCalc(void){
     signFinder('+');
     signFinder('-');
     
-    
     if (parenthesis_count == 0){
         printf("The result is %s\n", formula);
     }
     else {
+        // start a recursion if there is still parenthesis
         --parenthesis_count;
         priorityCalc();
     }
@@ -101,16 +112,13 @@ void parenthesisCounter(void){
         }
     }
     
+    // if parenthesis_count is odd there is an issue
     if (parenthesis_count%2 != 0){
         puts("Parenthesis count incorrect it won't work");
     }
+    // else we are good to go
     else{
         parenthesis_count = parenthesis_count / 2;
-        if (parenthesis_count == 0){
-            for (counter = 0; counter>formula_size; counter++){
-                expression[counter] = formula[counter];
-            }
-        }
         priorityCalc();
     }
 }
